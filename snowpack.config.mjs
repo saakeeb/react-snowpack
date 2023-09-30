3a69ac62b826ea9491d53587690780c511c15121
saakeeb <sakib14april@gmail.com>
2023-09-30 18:45:33 +0600

    faet: snowpack builder

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    // directory name: 'build directory'
+    public: "/",
+    src: "/dist",
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ["@snowpack/plugin-react-refresh"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
